1c8cfec8dc3d05b2c6935bf1aa90ffd5edd354e1
Etel7 <etel.leport@etudiant.univ-rennes.fr>
2026-04-22 13:25:57 +0900

    merge & bucket sort parallelized

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -1,12 +1,13 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <omp.h>
 
 void merge(std::vector<int>& vec, int begin, int mid, int end) {
   std::vector<int> tmp(end-begin+1);
   int left = begin;
   int right = mid+1;
-  for (int i=0; i<tmp.size(); i++) { 
+  for (int i=0; i<(int)tmp.size(); i++) { 
     if (left > mid)
       tmp[i] = vec[right++];
     else if (right > end)
@@ -16,15 +17,27 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
     else
       tmp[i] = vec[right++]; 
   }
-  for (int i=0; i<tmp.size(); i++) 
+  for (int i=0; i<(int)tmp.size(); i++) 
     vec[begin++] = tmp[i];
 }
 
 void merge_sort(std::vector<int>& vec, int begin, int end) {
-  if(begin < end) {
+  if (begin < end) {
     int mid = (begin + end) / 2;
-    merge_sort(vec, begin, mid);
-    merge_sort(vec, mid+1, end);
+
+    if (end - begin > 1000) {
+      #pragma omp task
+      merge_sort(vec, begin, mid);
+
+      #pragma omp task
+      merge_sort(vec, mid+1, end);
+
+      #pragma omp taskwait
+    } else {
+      merge_sort(vec, begin, mid);
+      merge_sort(vec, mid+1, end);
+    }
+
     merge(vec, begin, mid, end);
   }
 }
@@ -34,12 +47,18 @@ int main() {
   std::vector<int> vec(n);
   for (int i=0; i<n; i++) {
     vec[i] = rand() % (10 * n);
-    printf("%d ",vec[i]);
+    printf("%d ", vec[i]);
   }
   printf("\n");
-  merge_sort(vec, 0, n-1);
+
+  #pragma omp parallel
+  {
+    #pragma omp single
+    merge_sort(vec, 0, n-1);
+  }
+
   for (int i=0; i<n; i++) {
-    printf("%d ",vec[i]);
+    printf("%d ", vec[i]);
   }
   printf("\n");
 }
--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <omp.h>
 
 int main() {
   int n = 50;
@@ -8,25 +9,46 @@ int main() {
   std::vector<int> key(n);
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
-    printf("%d ",key[i]);
+    printf("%d ", key[i]);
   }
   printf("\n");
 
-  std::vector<int> bucket(range,0); 
-  for (int i=0; i<n; i++)
-    bucket[key[i]]++;
-  std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
+  int num_threads;
+  std::vector<int> bucket(range, 0);
+
+  // Comptage parallèle avec reduction
+  #pragma omp parallel
+  {
+    #pragma omp single
+    num_threads = omp_get_num_threads();
+
+    std::vector<int> local_bucket(range, 0);
+
+    #pragma omp for
+    for (int i=0; i<n; i++)
+      local_bucket[key[i]]++;
+
+    #pragma omp critical
+    for (int i=0; i<range; i++)
+      bucket[i] += local_bucket[i];
+  }
+
+  // Calcul des offsets (séquentiel, dépendance de données)
+  std::vector<int> offset(range, 0);
+  for (int i=1; i<range; i++)
     offset[i] = offset[i-1] + bucket[i-1];
+
+  // Réécriture parallèle
+  #pragma omp parallel for
   for (int i=0; i<range; i++) {
     int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
+    for (int k=0; k<bucket[i]; k++) {
       key[j++] = i;
     }
   }
 
   for (int i=0; i<n; i++) {
-    printf("%d ",key[i]);
+    printf("%d ", key[i]);
   }
   printf("\n");
 }
