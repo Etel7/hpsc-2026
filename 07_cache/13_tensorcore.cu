380bbff4ea4ac414557257d548b31a19b3b8c497
Etel7 <etel.leport@etudiant.univ-rennes.fr>
2026-06-06 17:54:01 +0900

    modif tensorcore

--- 07_cache/13_tensorcore.cu
+++ 07_cache/13_tensorcore.cu
@@ -13,7 +13,7 @@ using namespace nvcuda;
 
 const int TILE_SIZE = 128;
 
-__global__ void kernel_optimized_v12(int dim_m, int dim_n, int dim_k,
+__global__ void kernel_optimized_v13(int dim_m, int dim_n, int dim_k,
                                      const float * __restrict__ d_a, 
                                      const float * __restrict__ d_b, 
                                      float *d_c) {
@@ -25,7 +25,7 @@ __global__ void kernel_optimized_v12(int dim_m, int dim_n, int dim_k,
     int warp_m = warp_id / 4; 
     int warp_n = warp_id % 4; 
 
-    // Augmentation de la taille K à 32 pour l'alignement des lignes de cache L2
+    // Double buffering sur la dimension d'étape [2]
     __shared__ half block_a[2][32][TILE_SIZE + 8];
     __shared__ half block_b[2][TILE_SIZE][32 + 8]; 
 
@@ -39,96 +39,153 @@ __global__ void kernel_optimized_v12(int dim_m, int dim_n, int dim_k,
         }
     }
 
+    // --- PRE-CHARGE INITIALE (Etape k = 0) ---
     int write_stage = 0;
-
-    // On avance de 32 en 32
-    for (int k = 0; k < dim_k + 32; k += 32) {
-        
-        // 1. CHARGEMENT GLOBAL -> PARTAGÉ (4 étapes pour couvrir K=32 avec 256 threads)
-        if (k < dim_k) {
-            #pragma unroll
-            for (int step = 0; step < 4; ++step) {
-                int total_f4 = threadIdx.x + step * 256; 
-                int row_k = total_f4 / 32;               
-                int col_m = (total_f4 % 32) * 4;         
-                
-                int global_m = offset_a_m + col_m;
-                int global_k = k + row_k;
-                
-                if (global_m < dim_m && global_k < dim_k) {
-                    float4 tmp = reinterpret_cast<const float4*>(&d_a[global_k * dim_m + global_m])[0];
-                    block_a[write_stage][row_k][col_m + 0] = __float2half(tmp.x);
-                    block_a[write_stage][row_k][col_m + 1] = __float2half(tmp.y);
-                    block_a[write_stage][row_k][col_m + 2] = __float2half(tmp.z);
-                    block_a[write_stage][row_k][col_m + 3] = __float2half(tmp.w);
-                } else {
-                    block_a[write_stage][row_k][col_m + 0] = __float2half(0.0f);
-                    block_a[write_stage][row_k][col_m + 1] = __float2half(0.0f);
-                    block_a[write_stage][row_k][col_m + 2] = __float2half(0.0f);
-                    block_a[write_stage][row_k][col_m + 3] = __float2half(0.0f);
-                }
-            }
-
-            #pragma unroll
-            for (int step = 0; step < 4; ++step) {
-                int total_f4 = threadIdx.x + step * 256; 
-                int row_k = (total_f4 % 8) * 4;          // 8 threads consécutifs lisent 32 floats d'une colonne 
-                int col_n = total_f4 / 8;                
-                
-                int global_n = offset_b_n + col_n;
-                int global_k = k + row_k;
-                
-                if (global_n < dim_n && global_k < dim_k) {
-                    float4 tmp = reinterpret_cast<const float4*>(&d_b[global_n * dim_k + global_k])[0];
-                    // Transaction mémoire de 128 octets parfaite par colonne lue
-                    block_b[write_stage][col_n][row_k + 0] = __float2half(tmp.x);
-                    block_b[write_stage][col_n][row_k + 1] = __float2half(tmp.y);
-                    block_b[write_stage][col_n][row_k + 2] = __float2half(tmp.z);
-                    block_b[write_stage][col_n][row_k + 3] = __float2half(tmp.w);
-                } else {
-                    block_b[write_stage][col_n][row_k + 0] = __float2half(0.0f);
-                    block_b[write_stage][col_n][row_k + 1] = __float2half(0.0f);
-                    block_b[write_stage][col_n][row_k + 2] = __float2half(0.0f);
-                    block_b[write_stage][col_n][row_k + 3] = __float2half(0.0f);
-                }
+    {
+        #pragma unroll
+        for (int step = 0; step < 4; ++step) {
+            int total_f4 = threadIdx.x + step * 256; 
+            int row_k = total_f4 / 32;               
+            int col_m = (total_f4 % 32) * 4;         
+            int global_m = offset_a_m + col_m;
+            if (global_m < dim_m && row_k < dim_k) {
+                float4 tmp = reinterpret_cast<const float4*>(&d_a[row_k * dim_m + global_m])[0];
+                block_a[write_stage][row_k][col_m + 0] = __float2half(tmp.x);
+                block_a[write_stage][row_k][col_m + 1] = __float2half(tmp.y);
+                block_a[write_stage][row_k][col_m + 2] = __float2half(tmp.z);
+                block_a[write_stage][row_k][col_m + 3] = __float2half(tmp.w);
+            } else {
+                block_a[write_stage][row_k][col_m + 0] = __float2half(0.0f);
+                block_a[write_stage][row_k][col_m + 1] = __float2half(0.0f);
+                block_a[write_stage][row_k][col_m + 2] = __float2half(0.0f);
+                block_a[write_stage][row_k][col_m + 3] = __float2half(0.0f);
             }
         }
 
-        __syncthreads();
+        #pragma unroll
+        for (int step = 0; step < 4; ++step) {
+            int total_f4 = threadIdx.x + step * 256; 
+            int row_k = (total_f4 % 8) * 4;          
+            int col_n = total_f4 / 8;                
+            int global_n = offset_b_n + col_n;
+            if (global_n < dim_n && row_k < dim_k) {
+                float4 tmp = reinterpret_cast<const float4*>(&d_b[global_n * dim_k + row_k])[0];
+                block_b[write_stage][col_n][row_k + 0] = __float2half(tmp.x);
+                block_b[write_stage][col_n][row_k + 1] = __float2half(tmp.y);
+                block_b[write_stage][col_n][row_k + 2] = __float2half(tmp.z);
+                block_b[write_stage][col_n][row_k + 3] = __float2half(tmp.w);
+            } else {
+                block_b[write_stage][col_n][row_k + 0] = __float2half(0.0f);
+                block_b[write_stage][col_n][row_k + 1] = __float2half(0.0f);
+                block_b[write_stage][col_n][row_k + 2] = __float2half(0.0f);
+                block_b[write_stage][col_n][row_k + 3] = __float2half(0.0f);
+            }
+        }
+    }
+    __syncthreads();
 
-        // 2. CALCULS TENSOR CORES (Déroulés sur 2 sous-étapes de 16)
-        if (k > 0) {
-            int read_stage = 1 - write_stage;
+    // --- BOUCLE PRINCIPALE PIPELINÉE ---
+    for (int k = 32; k < dim_k; k += 32) {
+        int read_stage = write_stage;
+        write_stage = 1 - write_stage;
+
+        // 1. Lancement asynchrone / anticipé du chargement du PROCHAIN bloc (k) pendant que l'actuel calcule
+        #pragma unroll
+        for (int step = 0; step < 4; ++step) {
+            int total_f4 = threadIdx.x + step * 256; 
+            int row_k = total_f4 / 32;               
+            int col_m = (total_f4 % 32) * 4;         
+            int global_m = offset_a_m + col_m;
+            int global_k = k + row_k;
+            if (global_m < dim_m && global_k < dim_k) {
+                float4 tmp = reinterpret_cast<const float4*>(&d_a[global_k * dim_m + global_m])[0];
+                block_a[write_stage][row_k][col_m + 0] = __float2half(tmp.x);
+                block_a[write_stage][row_k][col_m + 1] = __float2half(tmp.y);
+                block_a[write_stage][row_k][col_m + 2] = __float2half(tmp.z);
+                block_a[write_stage][row_k][col_m + 3] = __float2half(tmp.w);
+            } else {
+                block_a[write_stage][row_k][col_m + 0] = __float2half(0.0f);
+                block_a[write_stage][row_k][col_m + 1] = __float2half(0.0f);
+                block_a[write_stage][row_k][col_m + 2] = __float2half(0.0f);
+                block_a[write_stage][row_k][col_m + 3] = __float2half(0.0f);
+            }
+        }
+
+        #pragma unroll
+        for (int step = 0; step < 4; ++step) {
+            int total_f4 = threadIdx.x + step * 256; 
+            int row_k = (total_f4 % 8) * 4;          
+            int col_n = total_f4 / 8;                
+            int global_n = offset_b_n + col_n;
+            int global_k = k + row_k;
+            if (global_n < dim_n && global_k < dim_k) {
+                float4 tmp = reinterpret_cast<const float4*>(&d_b[global_n * dim_k + global_k])[0];
+                block_b[write_stage][col_n][row_k + 0] = __float2half(tmp.x);
+                block_b[write_stage][col_n][row_k + 1] = __float2half(tmp.y);
+                block_b[write_stage][col_n][row_k + 2] = __float2half(tmp.z);
+                block_b[write_stage][col_n][row_k + 3] = __float2half(tmp.w);
+            } else {
+                block_b[write_stage][col_n][row_k + 0] = __float2half(0.0f);
+                block_b[write_stage][col_n][row_k + 1] = __float2half(0.0f);
+                block_b[write_stage][col_n][row_k + 2] = __float2half(0.0f);
+                block_b[write_stage][col_n][row_k + 3] = __float2half(0.0f);
+            }
+        }
+
+        // 2. CALCULS SUR LE STAGE ACTUEL (Pendant que la mémoire globale voyage vers le second stage)
+        #pragma unroll
+        for (int ki = 0; ki < 2; ki++) {
+            int k_offset = ki * 16;
+            wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::col_major> b_frag[2];
+            #pragma unroll
+            for (int c = 0; c < 2; c++) {
+                int col_tile = warp_n * 2 + c;
+                wmma::load_matrix_sync(b_frag[c], &block_b[read_stage][col_tile * 16][k_offset], 32 + 8);
+            }
 
             #pragma unroll
-            for (int ki = 0; ki < 2; ki++) {
-                int k_offset = ki * 16;
+            for (int r = 0; r < 4; r++) {
+                int row_tile = warp_m * 4 + r;
+                wmma::fragment<wmma::matrix_a, 16, 16, 16, half, wmma::col_major> a_frag;
+                wmma::load_matrix_sync(a_frag, &block_a[read_stage][k_offset][row_tile * 16], TILE_SIZE + 8);
 
-                wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::col_major> b_frag[2];
                 #pragma unroll
                 for (int c = 0; c < 2; c++) {
-                    int col_tile = warp_n * 2 + c;
-                    wmma::load_matrix_sync(b_frag[c], &block_b[read_stage][col_tile * 16][k_offset], 32 + 8);
-                }
-
-                #pragma unroll
-                for (int r = 0; r < 4; r++) {
-                    int row_tile = warp_m * 4 + r;
-                    wmma::fragment<wmma::matrix_a, 16, 16, 16, half, wmma::col_major> a_frag;
-                    wmma::load_matrix_sync(a_frag, &block_a[read_stage][k_offset][row_tile * 16], TILE_SIZE + 8);
-
-                    #pragma unroll
-                    for (int c = 0; c < 2; c++) {
-                        wmma::mma_sync(acc[r][c], a_frag, b_frag[c], acc[r][c]);
-                    }
+                    wmma::mma_sync(acc[r][c], a_frag, b_frag[c], acc[r][c]);
                 }
             }
         }
-        
-        write_stage = 1 - write_stage;
+
+        // On synchronise avant de switcher les buffers au prochain tour
+        __syncthreads();
     }
 
-    // ÉCRITURE FINALE
+    // --- DERNIER TOUR DE CALCULS (Épuisement du pipeline) ---
+    int read_stage = write_stage;
+    #pragma unroll
+    for (int ki = 0; ki < 2; ki++) {
+        int k_offset = ki * 16;
+        wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::col_major> b_frag[2];
+        #pragma unroll
+        for (int c = 0; c < 2; c++) {
+            int col_tile = warp_n * 2 + c;
+            wmma::load_matrix_sync(b_frag[c], &block_b[read_stage][col_tile * 16][k_offset], 32 + 8);
+        }
+
+        #pragma unroll
+        for (int r = 0; r < 4; r++) {
+            int row_tile = warp_m * 4 + r;
+            wmma::fragment<wmma::matrix_a, 16, 16, 16, half, wmma::col_major> a_frag;
+            wmma::load_matrix_sync(a_frag, &block_a[read_stage][k_offset][row_tile * 16], TILE_SIZE + 8);
+
+            #pragma unroll
+            for (int c = 0; c < 2; c++) {
+                wmma::mma_sync(acc[r][c], a_frag, b_frag[c], acc[r][c]);
+            }
+        }
+    }
+
+    // --- ÉCRITURE FINALE ---
     #pragma unroll
     for (int r = 0; r < 4; r++) {
         #pragma unroll
@@ -176,7 +233,7 @@ int main(int argc, const char **argv) {
 
     for (int i = 0; i < Nt+2; i++) {
         if (i == 2) tic = chrono::steady_clock::now();
-        kernel_optimized_v12<<< grid, block >>>(m, n, k, A, B, C2);
+        kernel_optimized_v13<<< grid, block >>>(m, n, k, A, B, C2);
         cudaDeviceSynchronize();
     }
     toc = chrono::steady_clock::now();
